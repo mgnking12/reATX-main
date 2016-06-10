0997d234495770e0b6e3396dc0aa4f31c832c0a4
Megan King <mgnking12@gmail.com>
2016-06-09 21:40:40 -0500

    jquery works on binButton

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,25 +3,26 @@
 
 $("body").on('click', '.binButton', function(event) {
 	event.preventDefault();
-	$("#contributeSection").data("sec", "signUp");
-	$("#binSection").data("sec", "signUp");
-	$("#logo-container").data("sec", "signUp");
-	if ("data-sec" == "signUp") {
+	console.log("bins")
+	$("#contributeSection").attr("data-sec", "bin");
+	$("#signUpSection").attr("data-sec", "bin");
+	$("#index-banner").attr("data-sec", "bin");
+	if ($("#binSection").attr("data-sec") == "signUp") {
 	$("#signUpSection").hide( "slide", 2000)
 	.delay(0)
 		.queue(function(n){
 			$("#binSection").fadeIn(0);
 			n();
 		});
-	}else if ("data-sec" == "contribute") {
+	}else if ($("#binSection").attr("data-sec") == "contribute") {
 	$("#contributeSection").hide( "slide", 2000)
 	.delay(0)
 		.queue(function(n){
 			$("#binSection").fadeIn(0);
 			n();
 		});
-	}else if ("data-sec" == "logoContainer") {
-	$("#logo-container").hide( "slide", 2000)
+	}else if ($("#binSection").attr("data-sec") == "index-banner") {
+	$("#index-banner").hide( "slide", 2000)
 	.delay(0)
 		.queue(function(n){
 			$("#binSection").fadeIn(0);
@@ -33,7 +34,7 @@ $("body").on('click', '.contributeButton', function(event) {
 	event.preventDefault();
 	$("#contributeSection").data("sec", "contribute");
 	$("#binSection").data("sec", "contribute");
-	$("#logo-container").data("sec", "contribute");
+	$("#index-banner").data("sec", "contribute");
 	if ("data-sec" == "bin") {
 	$("#binSection").hide( "slide", 2000)
 	.delay(0)
@@ -48,8 +49,8 @@ $("body").on('click', '.contributeButton', function(event) {
 			$("#contributeSection").fadeIn(0);
 			n();
 		});
-	}else if ("data-sec" == "logoContainer") {
-	$("#logo-container").hide( "slide", 2000)
+	}else if ("data-sec" == "index-banner") {
+	$("#index-banner").hide( "slide", 2000)
 	.delay(0)
 		.queue(function(n){
 			$("#contributeSection").fadeIn(0);
@@ -61,7 +62,7 @@ $("body").on('click', '.signUpButton', function(event) {
 	event.preventDefault();
 	$("#contributeSection").data("sec", "signUp");
 	$("#binSection").data("sec", "signUp");
-	$("#logo-container").data("sec", "signUp");
+	$("#index-banner").data("sec", "signUp");
 	if ("data-sec" == "bin") {
 	$("#binSection").hide( "slide", 2000)
 	.delay(0)
@@ -76,8 +77,8 @@ $("body").on('click', '.signUpButton', function(event) {
 			$("#signUpSection").fadeIn(0);
 			n();
 		});
-	}else if ("data-sec" == "logoContainer") {
-	$("#logo-container").hide( "slide", 2000)
+	}else if ("data-sec" == "index-banner") {
+	$("#index-banner").hide( "slide", 2000)
 	.delay(0)
 		.queue(function(n){
 			$("#signUpSection").fadeIn(0);
@@ -87,10 +88,10 @@ $("body").on('click', '.signUpButton', function(event) {
 });
 $("body").on('click', '#logo-container', function(event) {
 	event.preventDefault();
-	$("#contributeSection").data("sec", "logoContainer");
-	$("#binSection").data("sec", "logoContainer");
-	$("#signUpSection").data("sec", "logoContainer");
-	$("#logo-container").show( "slide", 2000);
+	$("#contributeSection").data("sec", "index-banner");
+	$("#binSection").data("sec", "index-banner");
+	$("#signUpSection").data("sec", "index-banner");
+	$("#index-banner").show( "slide", 2000);
 			$("#binSection").css('display', 'none');
 			$("#signUpSection").css('display', 'none');
 			$("#contributeSection").css('display', 'none');
