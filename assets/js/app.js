630f4f54075128aeaa23b58413c0485c2622f655
Megan King <mgnking12@gmail.com>
2016-06-09 21:07:21 -0500

    jquery doesn't work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,99 @@
 
   $(document).ready(function(){
 
-$("body").on('click', '.bin-button', function(event) {
+$("body").on('click', '.binButton', function(event) {
 	event.preventDefault();
-	$("#index-banner").hide( "slide", 2000)
+	$("#contributeSection").data("sec", "signUp");
+	$("#binSection").data("sec", "signUp");
+	$("#logo-container").data("sec", "signUp");
+	if ("data-sec" == "signUp") {
+	$("#signUpSection").hide( "slide", 2000)
 	.delay(0)
 		.queue(function(n){
 			$("#binSection").fadeIn(0);
 			n();
 		});
+	}else if ("data-sec" == "contribute") {
+	$("#contributeSection").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#binSection").fadeIn(0);
+			n();
+		});
+	}else if ("data-sec" == "logoContainer") {
+	$("#logo-container").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#binSection").fadeIn(0);
+			n();
+		});
+	}
+});
+$("body").on('click', '.contributeButton', function(event) {
+	event.preventDefault();
+	$("#contributeSection").data("sec", "contribute");
+	$("#binSection").data("sec", "contribute");
+	$("#logo-container").data("sec", "contribute");
+	if ("data-sec" == "bin") {
+	$("#binSection").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#contributeSection").fadeIn(0);
+			n();
+		});
+	}else if ("data-sec" == "signUp") {
+	$("#signUpSection").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#contributeSection").fadeIn(0);
+			n();
+		});
+	}else if ("data-sec" == "logoContainer") {
+	$("#logo-container").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#contributeSection").fadeIn(0);
+			n();
+		});
+	}
+});
+$("body").on('click', '.signUpButton', function(event) {
+	event.preventDefault();
+	$("#contributeSection").data("sec", "signUp");
+	$("#binSection").data("sec", "signUp");
+	$("#logo-container").data("sec", "signUp");
+	if ("data-sec" == "bin") {
+	$("#binSection").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#signUpSection").fadeIn(0);
+			n();
+		});
+	}else if ("data-sec" == "contribute") {
+	$("#contributeSection").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#signUpSection").fadeIn(0);
+			n();
+		});
+	}else if ("data-sec" == "logoContainer") {
+	$("#logo-container").hide( "slide", 2000)
+	.delay(0)
+		.queue(function(n){
+			$("#signUpSection").fadeIn(0);
+			n();
+		});
+	}
 });
 $("body").on('click', '#logo-container', function(event) {
 	event.preventDefault();
-	$("#index-banner").show( "slide", 2000);
-			$("#binSection").css('display', 'none', 2000);
+	$("#contributeSection").data("sec", "logoContainer");
+	$("#binSection").data("sec", "logoContainer");
+	$("#signUpSection").data("sec", "logoContainer");
+	$("#logo-container").show( "slide", 2000);
+			$("#binSection").css('display', 'none');
+			$("#signUpSection").css('display', 'none');
+			$("#contributeSection").css('display', 'none');
 });
 
 
