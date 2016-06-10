684f4c481e0f4e76013351ded7f3f75b65124973
Megan King <mgnking12@gmail.com>
2016-06-10 00:01:30 -0500

    google maps

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,43 @@
 
-  $(document).ready(function(){
 
+
+
+  $(document).ready(function(){
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
 $("body").on('click', '.binButton', function(event) {
 	event.preventDefault();
+	initMap();
 	console.log("bins")
 	$("#contributeSection").attr("data-sec", "bin");
 	$("#signUpSection").attr("data-sec", "bin");
