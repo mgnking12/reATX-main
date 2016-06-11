2dd416259261c8807b4de938b4391c1b86d749a9
Megan King <mgnking12@gmail.com>
2016-06-11 13:40:30 -0500

    ui fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,49 @@
 $(document).ready(function() {
 
-function initMap() {
+    function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 17
+            center: {
+                lat: -34.397,
+                lng: 150.644
+            },
+            zoom: 17
         });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+        var infoWindow = new google.maps.InfoWindow({
+            map: map
+        });
 
-         // This event listener calls addMarker() when the map is clicked.
+        // This event listener calls addMarker() when the map is clicked.
         google.maps.event.addListener(map, 'click', function(event) {
-        placeMarker(event.latLng);
+            placeMarker(event.latLng);
         });
 
         function placeMarker(location) {
             var marker = new google.maps.Marker({
-                position: location, 
+                position: location,
                 map: map
             });
         }
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
         } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
         }
-      }
+    }
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
@@ -49,40 +55,39 @@ function initMap() {
 
 
     $("body").on('click', '.binButton', function(event) {
-    	 initMap();
+
         event.preventDefault();
         console.log("bins")
         $("#contributeSection").attr("data-sec", "bin");
         $("#signUpSection").attr("data-sec", "bin");
         $("#index-banner").attr("data-sec", "bin");
-        if ($("#binSection").attr("data-sec") == "signUp") {
-            $("#signUpSection").hide("slide", 2000)
-                .delay(0)
-                .queue(function(n) {
-                    $("#binSection").fadeIn(0);
-                    n();
+
+        function showMap(currentSection) {
+            if (currentSection == "#binSection") {
+
+            } else {
+                $(currentSection).hide({
+                    event: "slide",
+                    duration: 2000,
+                    complete: function() {
+                        $("#binSection").fadeIn(0);
+                        initMap();
+                    }
                 });
-        } else if ($("#binSection").attr("data-sec") == "contribute") {
-            $("#contributeSection").hide("slide", 2000)
-                .delay(0)
-                .queue(function(n) {
-                    $("#binSection").fadeIn(0);
-                    n();
-                });
-        } else if ($("#binSection").attr("data-sec") == "index-banner") {
-            $("#index-banner").hide("slide", 2000)
-                .delay(0)
-                .queue(function(n) {
-                    $("#binSection").fadeIn(0);
-                    n();
-                });
+            }
         }
-        //$("#binSection").append("<div class='col s1' id='sideBarComments'>")
+        if ($("#binSection").attr("data-sec") == "signUp") {
+            showMap('#signUpSection');
+        } else if ($("#binSection").attr("data-sec") == "contribute") {
+            showMap('#contributeSection');
+        } else if ($("#binSection").attr("data-sec") == "index-banner") {
+            showMap('#index-banner')
+        }
     });
     $("body").on('click', '.bbuttonIcon', function(event) {
         event.preventDefault();
         $("#sideBarComments").removeClass("hoverable");
-        $(".bbuttonIcon").css('display', 'none');        
+        $(".bbuttonIcon").css('display', 'none');
         $("#sideBarComments").animate({
             marginTop: '0%',
             marginLeft: '-30%',
@@ -92,32 +97,32 @@ function initMap() {
             backgroundColor: '#2980b9',
             backgroundColor: 'rgba(41, 128, 185, 0.6)',
         }, 1000).delay(0)
-                .queue(function(n) {
-        $("#sideBarComments").removeClass("valign-wrapper hoverable");
+            .queue(function(n) {
+                $("#sideBarComments").removeClass("valign-wrapper hoverable");
 
-        $(".xbuttonIcon").css('display', 'inline-block');
-        $(".popout").css('display', 'inline-block');
-             n();
-                });
+                $(".xbuttonIcon").css('display', 'inline-block');
+                $(".popout").css('display', 'inline-block');
+                n();
+            });
     });
     $("body").on('click', '.xbi', function(event) {
-    	 event.preventDefault();
-    	 $("#sideBarComments").removeClass("hoverable");
-    	$(".xbuttonIcon").css('display', 'none');
-    	$(".popout").css('display', 'none');
+        event.preventDefault();
+        $("#sideBarComments").removeClass("hoverable");
+        $(".xbuttonIcon").css('display', 'none');
+        $(".popout").css('display', 'none');
         $("#sideBarComments").animate({
-        	marginLeft: '0',
-        	height: '50px',
-	        width: '50px',
-	        marginTop: '300px',
-	        backgroundColor: '#2980b9',
-	        borderRadius: '50%',
+            marginLeft: '0',
+            height: '50px',
+            width: '50px',
+            marginTop: '300px',
+            backgroundColor: '#2980b9',
+            borderRadius: '50%',
         }, 1000).delay(0)
-                .queue(function(n) {                 
-        $("#sideBarComments").addClass("valign-wrapper hoverable");
-        $(".bbuttonIcon").css('display', 'inline-block');
-                    n();
-                });
+            .queue(function(n) {
+                $("#sideBarComments").addClass("valign-wrapper hoverable");
+                $(".bbuttonIcon").css('display', 'inline-block');
+                n();
+            });
     });
     $("body").on('mouseover', '.xbuttonIcon', function(event) {
         $("#sideBarComments").addClass("hoverable");
@@ -127,7 +132,30 @@ function initMap() {
         $("#binSection").attr("data-sec", "contribute");
         $("#signUpSection").attr("data-sec", "contribute");
         $("#index-banner").attr("data-sec", "contribute");
+
+        function showMap(currentSection) {
+            if (currentSection == "#contributeSection") {
+
+            } else {
+                $(currentSection).hide({
+                    event: "slide",
+                    duration: 2000,
+                    complete: function() {
+                        $("#contributeSection").fadeIn(0);
+                        initMap();
+                    }
+                });
+            }
+        }
         if ($("#contributeSection").attr("data-sec") == "signUp") {
+            showMap('#signUpSection');
+        } else if ($("#contributeSection").attr("data-sec") == "bin") {
+            showMap('#binSection');
+        } else if ($("#contributeSection").attr("data-sec") == "index-banner") {
+            showMap('#index-banner')
+        }
+
+        /*if ($("#contributeSection").attr("data-sec") == "signUp") {
             $("#signUpSection").hide("slide", 2000)
                 .delay(0)
                 .queue(function(n) {
@@ -148,34 +176,34 @@ function initMap() {
                     $("#contributeSection").fadeIn(0);
                     n();
                 });
-        }
+        }*/
     });
     $("body").on('click', '.signUpButton', function(event) {
         event.preventDefault();
         $("#binSection").attr("data-sec", "signUp");
         $("#contributeSection").attr("data-sec", "signUp");
         $("#index-banner").attr("data-sec", "signUp");
+
+        function showMap(currentSection) {
+            if (currentSection == "#signUpSection") {
+
+            } else {
+                $(currentSection).hide({
+                    event: "slide",
+                    duration: 2000,
+                    complete: function() {
+                        $("#signUpSection").fadeIn(0);
+                        initMap();
+                    }
+                });
+            }
+        }
         if ($("#signUpSection").attr("data-sec") == "contribute") {
-            $("#contributeSection").hide("slide", 2000)
-                .delay(0)
-                .queue(function(n) {
-                    $("#signUpSection").fadeIn(0);
-                    n();
-                });
+            showMap('#contributeSection');
         } else if ($("#signUpSection").attr("data-sec") == "bin") {
-            $("#binSection").hide("slide", 2000)
-                .delay(0)
-                .queue(function(n) {
-                    $("#signUpSection").fadeIn(0);
-                    n();
-                });
+            showMap('#binSection');
         } else if ($("#signUpSection").attr("data-sec") == "index-banner") {
-            $("#index-banner").hide("slide", 2000)
-                .delay(0)
-                .queue(function(n) {
-                    $("#signUpSection").fadeIn(0);
-                    n();
-                });
+            showMap('#index-banner')
         }
     });
     $("body").on('click', '#logo-container', function(event) {
@@ -187,22 +215,22 @@ function initMap() {
         $("#binSection").css('display', 'none');
         $("#signUpSection").css('display', 'none');
         $("#contributeSection").css('display', 'none');
-    	$("#sideBarComments").removeClass("hoverable");
-    	$(".xbuttonIcon").css('display', 'none');
-    	$(".popout").css('display', 'none');
+        $("#sideBarComments").removeClass("hoverable");
+        $(".xbuttonIcon").css('display', 'none');
+        $(".popout").css('display', 'none');
         $("#sideBarComments").animate({
-        	marginLeft: '0',
-        	height: '50px',
-	        width: '50px',
-	        marginTop: '300px',
-	        backgroundColor: '#2980b9',
-	        borderRadius: '50%',
+            marginLeft: '0',
+            height: '50px',
+            width: '50px',
+            marginTop: '300px',
+            backgroundColor: '#2980b9',
+            borderRadius: '50%',
         }, 1000).delay(0)
-                .queue(function(n) {                 
-        $("#sideBarComments").addClass("valign-wrapper hoverable");
-        $(".bbuttonIcon").css('display', 'inline-block');
-                    n();
-                });
+            .queue(function(n) {
+                $("#sideBarComments").addClass("valign-wrapper hoverable");
+                $(".bbuttonIcon").css('display', 'inline-block');
+                n();
+            });
     });
 
 
